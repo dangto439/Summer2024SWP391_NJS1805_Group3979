8dac0da70866faab282c5cc84a7d3e21f0c010a1
tsantriduc <tsantriduc@gmail.com>
2024-06-10 19:27:53 +0700

    upload contact homepage UI

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,8 @@ import { selectUser } from "./redux/features/counterSlice";
 import { Navigate } from "react-router-dom/dist";
 import { toast } from "react-toastify";
 import Profile from "./components/profile";
+import Intro from "./pages/intro";
+import Contact from "./pages/contact";
 
 function App() {
   const user = useSelector(selectUser);
@@ -21,7 +23,7 @@ function App() {
 
   const AuthRoute = ({ children }) => {
     if (user == null || user.role != "ADMIN") {
-      toast.error("m ko co quyen vao day th lz ");
+      toast.error("You are not Admin!");
       return <Navigate to="/login" />;
     }
     return children;
@@ -45,6 +47,22 @@ function App() {
           element: (
             // <PrivateRoute>
             <HomePage />
+            // </PrivateRoute>
+          ),
+        },
+        {
+          path: "/introduction",
+          element: (
+            // <PrivateRoute>
+            <Intro />
+            // </PrivateRoute>
+          ),
+        },
+        {
+          path: "/contact",
+          element: (
+            // <PrivateRoute>
+            <Contact />
             // </PrivateRoute>
           ),
         },
