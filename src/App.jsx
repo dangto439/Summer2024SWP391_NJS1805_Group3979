c0d2912fd58c0c8aa5e464472bcf41c5641c6ef6
tsantriduc <tsantriduc@gmail.com>
2024-06-18 00:46:09 +0700

    upload court detail of each club UI

--- src/App.jsx
+++ src/App.jsx
@@ -22,6 +22,7 @@ import Court from "../src/components/club";
 import Club from "../src/components/club";
 import Booking from "./pages/booking";
 import HistoryBooking from "./pages/history-booking";
+import CourtDetail from "./pages/court-detail";
 
 function App() {
   const user = useSelector(selectUser);
@@ -89,6 +90,14 @@ function App() {
           element: (
             // <PrivateRoute>
             <HistoryBooking />
+            // </PrivateRoute>
+          ),
+        },
+        {
+          path: "/court-detail",
+          element: (
+            // <PrivateRoute>
+            <CourtDetail />
             // </PrivateRoute>
           ),
         },
