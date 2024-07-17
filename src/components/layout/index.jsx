c7a8bb638ab124a8b8874745fe822e2d9961979a
tsantriduc <ducttse172760@fpt.edu.vn>
2024-07-17 23:04:57 +0700

    update balance change when transfer UI

--- src/components/layout/index.jsx
+++ src/components/layout/index.jsx
@@ -1,13 +1,30 @@
 import { Outlet } from "react-router-dom";
 import Header from "../header";
 import Footer from "../footer";
+import { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import { selectUser } from "../../redux/features/counterSlice";
+import api from "../../config/axios";
 
 function Layout() {
+  const [balance2, setBalance2] = useState(0);
+  const user = useSelector(selectUser);
+
+  const fetchBalance = async () => {
+    const response = await api.get(`/wallet/${user.id}`);
+    console.log(response);
+    setBalance2(response.data.balance);
+  };
+
+  useEffect(() => {
+    fetchBalance();
+  }, []);
+
   return (
     <div>
-      <Header />
+      <Header balance2={balance2} />
       <div>
-        <Outlet />
+        <Outlet context={{ setBalance2, balance2 }} />
       </div>
       <Footer />
     </div>
