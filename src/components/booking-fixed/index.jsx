95d4d2c6c9242c1ebfeea44f56e4c6ebba2c44e9
tsantriduc <ducttse172760@fpt.edu.vn>
2024-07-21 06:33:34 +0700

    update booking fixed page UI

--- src/components/booking-fixed/index.jsx
+++ src/components/booking-fixed/index.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/prop-types */
 import moment from "moment/moment";
 import "./index.scss";
 import { useEffect, useState } from "react";
@@ -11,6 +12,7 @@ import {
   Row,
   Radio,
   message,
+  Space,
 } from "antd";
 import api from "../../config/axios";
 import { useNavigate } from "react-router-dom";
@@ -168,60 +170,96 @@ function BookingFixed({ club }) {
         <Row>
           <Col span={24} className="booking-summary-fixed">
             <h1>{club.clubName}</h1>
-            <h1>
+            <p>
               Thời gian đặt:{" "}
               {selectedDate ? selectedDate.format("MM/YYYY") : ""}
-            </h1>
-            <img
-              src="https://firebasestorage.googleapis.com/v0/b/badminton-booking-platform.appspot.com/o/z5545153816126_834da2b1757f9fca8d39197a7ac64f93.jpg?alt=media&token=50c69782-7782-42c9-877d-c07a1e906abb"
-              alt=""
-            />
+            </p>
+            <img src={club.urlImages} />
             <Input
               placeholder="Nhập mã khuyến mãi"
               variant="borderless"
               value={promotionCode}
               onChange={handleInputChange}
             />
-            <Button className="submit-button-fixed" onClick={handleSubmit}>
-              THANH TOÁN
+            <Button
+              className="submit-button-fixed"
+              onClick={handleSubmit}
+              size="large"
+            >
+              Thanh Toán
             </Button>
           </Col>
         </Row>
       </Col>
       <Col span={17} className="booking-main-fixed">
-        <DatePicker
-          className="datepicker-fixed"
-          onChange={onChange}
-          disabledDate={disabledDate}
-          picker="month"
-        />
-        <Radio.Group
-          onChange={handleCourtOptionChange}
-          value={selectedCourtOption}
-        >
-          <Radio value="any">một sân bất kì</Radio>
-          <Radio value="specific">chọn sân mình muốn</Radio>
-        </Radio.Group>
-        {selectedCourtOption === "specific" && (
-          <Checkbox.Group options={options} onChange={onChangeSelectCourt} />
-        )}
-        <Checkbox.Group
-          onChange={handleDayChange}
-          options={dayOptions}
-          style={{ marginTop: 20 }}
-        />
-        <div className="slots-container">
-          {slots.map((slot) => (
-            <div
-              key={slot.slotId}
-              className={`slot-item ${
-                selectedSlots.includes(slot.slotId) ? "selected-slot" : ""
-              }`}
-              onClick={() => handleSlotSelect(slot.slotId)}
+        <h1>Lịch cố định</h1>
+        <div className="booking-month">
+          <h3>Chọn tháng:</h3>
+          <DatePicker
+            // style={{ width: 500 }}
+            className="datepicker-fixed"
+            onChange={onChange}
+            disabledDate={disabledDate}
+            picker="month"
+            placeholder="Chọn tháng..."
+          />
+        </div>
+
+        <h3>Chọn thứ:</h3>
+        <div className="booking-checkbox-date">
+          <Checkbox.Group
+            onChange={handleDayChange}
+            options={dayOptions}
+            style={{ marginTop: 20, fontSize: 20 }}
+          />
+        </div>
+
+        <div className="booking-fixed-contents">
+          <div className="booking-radio-court">
+            <h3>Tùy chọn sân:</h3>
+            <Radio.Group
+              className="radio"
+              onChange={handleCourtOptionChange}
+              value={selectedCourtOption}
+              size="large"
+              buttonStyle="solid"
+              optionType="button"
             >
-              {`${slot.slotId}:00`}
-            </div>
-          ))}
+              <Space direction="vertical">
+                <Radio.Button style={{ width: 250 }} value="any">
+                  Sắp xếp sân ngẫu nhiên
+                </Radio.Button>
+                <Radio.Button
+                  style={{ width: 250, marginBottom: 10 }}
+                  value="specific"
+                >
+                  Chọn sân muốn chơi
+                </Radio.Button>
+              </Space>
+            </Radio.Group>
+            {selectedCourtOption === "specific" && (
+              <Checkbox.Group
+                style={{ fontSize: 20 }}
+                options={options}
+                onChange={onChangeSelectCourt}
+              />
+            )}
+          </div>
+
+          <h3>Chọn giờ:</h3>
+          <div className="slots-container">
+            {slots.map((slot) => (
+              <div
+                key={slot.slotId}
+                className={`slot-item ${
+                  selectedSlots.includes(slot.slotId) ? "selected-slot" : ""
+                }`}
+                onClick={() => handleSlotSelect(slot.slotId)}
+              >
+                {`${slot.slotId}:00`}
+              </div>
+            ))}
+          </div>
         </div>
       </Col>
     </Row>
