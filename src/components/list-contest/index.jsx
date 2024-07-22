8b8fa6127417eb39fc0aad7991643c8f47e2d880
demi <phandiem0709@gmail.com>
2024-07-23 05:55:30 +0700

    additional const months

--- src/components/list-contest/index.jsx
+++ src/components/list-contest/index.jsx
@@ -160,6 +160,20 @@ const ListContest = () => {
     const year = date.getFullYear();
     return `${month} năm ${year}`;
   };
+  const months = [
+    "Tháng 1",
+    "Tháng 2",
+    "Tháng 3",
+    "Tháng 4",
+    "Tháng 5",
+    "Tháng 6",
+    "Tháng 7",
+    "Tháng 8",
+    "Tháng 9",
+    "Tháng 10",
+    "Tháng 11",
+    "Tháng 12",
+  ];
 
   const showModal = () => {
     setIsModalVisible(true);
